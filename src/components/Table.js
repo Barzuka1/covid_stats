a5536f56097466d03c228d80ec46c5a6916ee45f
Barzuka <67840224+Barzuka1@users.noreply.github.com>
2021-02-18 19:47:43 +0200

    Data Table

---
+++ src/components/Table.js
@@ -0,0 +1,61 @@
+import { DataGrid } from '@material-ui/data-grid'
+import React, { Component } from 'react'
+import axios from 'axios'
+
+const columns = [
+    { field: 'id', headerName: '#', width: 70 },
+    { field: 'Country', headerName: 'Country', width: 190 },
+    { field: 'TotalConfirmed', headerName: 'Cases', width: 130, type: 'number' },
+    { field: 'TotalDeaths', headerName: 'Deaths', width: 120, type: 'number' },
+    { field: 'TotalRecovered', headerName: 'Total Recovered', width: 170, type: 'number' },
+    { field: 'NewConfirmed', headerName: 'New Cases', width: 150, type: 'number' },
+    { field: 'NewDeaths', headerName: 'New Deaths', width: 150, type: 'number' },
+]
+  
+
+
+export class Table extends Component {
+
+    state = {
+        info: []
+    }
+
+    componentDidMount() {
+        axios.get("https://api.covid19api.com/summary")
+        .then(res => {
+            const { data } = res
+            let info = []
+            
+            data.Countries.forEach((country, iterator) => {
+                info.push({
+                    id: iterator + 1,
+                    'Country': country.Country,
+                    'NewConfirmed': country.NewConfirmed,
+                    'TotalConfirmed': country.TotalConfirmed,
+                    'NewDeaths': country.NewDeaths,
+                    'TotalDeaths': country.TotalDeaths,
+                    'TotalRecovered': country.TotalRecovered,
+                })
+            })
+
+            this.setState({ info })
+        })
+    }
+
+
+    render() {
+        return (
+            <div style={{ height: 700, width: '100%' }}>
+                <DataGrid
+                rows={this.state.info} 
+                columns={columns} 
+                pageSize={10} 
+                checkboxSelection 
+                rowsPerPageOptions={[10, 20, 50, 100, 200]} 
+                />
+            </div>
+        )
+    }
+}
+
+export default Table
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,24 @@
-import { Grid } from '@material-ui/core'
+import { Grid, Paper, Typography } from '@material-ui/core'
 import React from 'react'
 import Header from '../components/Header'
+import Table from '../components/Table'
+
 
 function Home() {
     return (
         <Grid container spacing={3}>
             <Grid item xs={12}>
                 <Header />
+            </Grid>
+            <Grid item xs={12}>
+                <Paper>
+                    <Typography align="center" variant="h4" color="inherit">
+                        Worldwide Statistics
+                    </Typography>
+                </Paper>
+            </Grid>
+            <Grid item xs={12}>
+                <Table />
             </Grid>
         </Grid>
     )
